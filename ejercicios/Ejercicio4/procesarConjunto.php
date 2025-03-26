e35db282b97251793d80201d47b51e60a2d26a1a
David Martinez <cdmartinez@uniboyaca.edu.co>
2025-03-26 14:56:39 -0500

    Critian: ejercicio 4

---
+++ ejercicios/Ejercicio4/procesarConjunto.php
@@ -0,0 +1,33 @@
+<?php
+require_once "Conjunto.php";
+require_once "ConjuntoOperaciones.php";
+
+function limpiarEntrada(string $input): array {
+  
+    $elementos = array_map('trim', explode(',', $input));
+
+    
+    $elementosValidos = array_filter($elementos, function($item) {
+        return preg_match('/^[a-zA-Z0-9]+$/', $item);
+    });
+
+    return array_values($elementosValidos); 
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $a = isset($_POST['conjunto_a']) ? limpiarEntrada($_POST['conjunto_a']) : [];
+    $b = isset($_POST['conjunto_b']) ? limpiarEntrada($_POST['conjunto_b']) : [];
+
+    $conjuntoA = new Conjunto($a);
+    $conjuntoB = new Conjunto($b);
+
+    $resultados = [
+        'union' => implode(', ', ConjuntoOperaciones::union($conjuntoA, $conjuntoB)->getElementos()),
+        'interseccion' => implode(', ', ConjuntoOperaciones::interseccion($conjuntoA, $conjuntoB)->getElementos()),
+        'diferencia_ab' => implode(', ', ConjuntoOperaciones::diferencia($conjuntoA, $conjuntoB)->getElementos()),
+        'diferencia_ba' => implode(', ', ConjuntoOperaciones::diferencia($conjuntoB, $conjuntoA)->getElementos())
+    ];
+
+    
+}
+?>
