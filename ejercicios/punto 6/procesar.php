96c4459a2f1ac970db6e7bc081e93d977f90bd10
David Martinez <cdmartinez@uniboyaca.edu.co>
2025-03-26 22:41:26 -0500

    Cristian: Correccion al imprimir arbol

--- ejercicios/punto 6/procesar.php
+++ ejercicios/punto 6/procesar.php
@@ -5,15 +5,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $preorden = isset($_POST['preorden']) ? explode(',', str_replace(' ', '', $_POST['preorden'])) : [];
     $inorden = isset($_POST['inorden']) ? explode(',', str_replace(' ', '', $_POST['inorden'])) : [];
 
-    if (count($preorden) > 0 && count($inorden) > 0) {
-        $arbol = new Arbol();
-        $arbol->raiz = $arbol->construirDesdePreInorden($preorden, $inorden);
-        session_start();
-        $_SESSION['arbol'] = serialize($arbol);
-        header("Location: mostrar_arbol.php");
+    if (empty($preorden) || empty($inorden)) {
+        echo "<h3>Error: Debe ingresar ambos recorridos.</h3>";
+        echo "<a href='index.html'><button>Volver</button></a>";
         exit();
-    } else {
-        echo "Por favor ingrese al menos dos recorridos.";
     }
+
+    $arbol = new Arbol();
+    $arbol->raiz = $arbol->construirDesdePreInorden($preorden, $inorden);
+    session_start();
+    $_SESSION['arbol'] = serialize($arbol);
+    header("Location: mostrar_arbol.php");
+    exit();
 }
 ?>
